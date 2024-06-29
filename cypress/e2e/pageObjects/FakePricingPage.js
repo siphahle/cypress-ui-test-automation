6813135573e125d0396b66073614f775ba63dca2
happy0412 <siphahle.nomnganga@gmail.com>
2024-06-29 22:52:02 +0200

    Added Fake Pricing Test

---
+++ cypress/e2e/pageObjects/FakePricingPage.js
@@ -0,0 +1,19 @@
+class FakePricingPage
+{
+    getTitle()
+    {
+      return cy.title().should('eq','Automation Practice - Ultimate QA')
+    }
+    clickOnFakePricingPage()
+    {
+      return  cy.get('.et_pb_text_inner > ul > :nth-child(3) > a')
+    }
+    verifyPurchaseBtns()
+    {
+        return cy.get('.et_pb_button et_pb_pricing_table_button').should('have.length.of.at.most', 3)
+
+    }
+   
+}
+
+export default FakePricingPage;
