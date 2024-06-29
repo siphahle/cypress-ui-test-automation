de55e913b41fbf67215afc415900598f6b307081
happy0412 <siphahle.nomnganga@gmail.com>
2024-06-29 22:15:50 +0200

    Added viewport configs

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
   env: {
     url: "https://ultimateqa.com"
   },
+  viewportWidth: 1280,
+  viewportHeight: 720,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
