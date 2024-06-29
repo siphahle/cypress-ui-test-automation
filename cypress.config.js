3f8e91dc749db661f2e5d24482eb34ef2e5ea358
happy0412 <siphahle.nomnganga@gmail.com>
2024-06-29 21:34:07 +0200

    Successfully Added Reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
   env: {
     url: "https://ultimateqa.com"
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
 
       
     },
-     specPattern: 'cypress/integration/examples/',
+     specPattern: 'cypress/e2e/examples/',
      chromeWebSecurity: false,
      testIsolation:false,
-    
   },
 });
